6f29ff053fd2eb962b395ac72e7bd0907ae4fc29
Sebas15897 <sebastiancontreras15897@gmail.com>
2022-01-29 16:10:40 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    PagesModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
